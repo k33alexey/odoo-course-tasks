6be42ff57db3eb75bdbe09c4b208ee5c9f9c98ab
Kalinin Alexey <k33alexey@gmail.com>
2026-04-20 12:11:55 +0300

    added description

--- addons/hr_hospital/__manifest__.py
+++ addons/hr_hospital/__manifest__.py
@@ -3,15 +3,7 @@
     'version': '1.0',
     'category': 'Healthcare',
     'summary': "Manage doctors, patients, diseases and hospital visits",
-    'description': """
-Hospital Management System
-==========================
-This module allows you to manage core hospital processes:
-**Doctors**: Professional profiles and specializations.
-**Patients**: Records and medical history.
-**Diseases**: Directory of medical conditions.
-**Visits**: Tracking patient appointments and consultations.
-    """,
+    'description': "",
     'author': "Oleksii Kalinin",
     'website': "https://github.com/k33alexey/odoo-school",
     'license': "OPL-1",
