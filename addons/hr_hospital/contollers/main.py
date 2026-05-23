ea3168acfd7ec234d2c2d351b5bd2ffafd981fc0
Kalinin Alexey <k33alexey@gmail.com>
2026-05-23 11:39:38 +0300

    modified

--- addons/hr_hospital/contollers/main.py
+++ addons/hr_hospital/contollers/main.py
@@ -1,6 +1,3 @@
-import json
-from http import HTTPStatus
-
 from odoo import http
 from odoo.http import request
 
@@ -8,7 +5,7 @@ from odoo.http import request
 class HospitalController(http.Controller):
     """Controller responsible for handling HTTP API requests related to the hospital module."""
 
-    @http.route(route='/api/hospital/doctor', type='http', website=True, auth='public', methods=['GET'], csrf=False)
+    @http.route(route='/api/hospital/doctor', type='jsonrpc', auth='public', methods=['GET'], csrf=False)
     def get_doctors(self):
         """Retrieve a list of all doctors in the system.
 
@@ -28,11 +25,11 @@ class HospitalController(http.Controller):
             for doctor in doctors
         ]
 
-        return request.make_response(
-            json.dumps({'status': 'success', 'data': result}), headers=[('Content-Type', 'application/json')]
-        )
+        return {'status': 'success', 'data': result}
 
-    @http.route(route='/api/hospital/doctor/<int:doctor_id>', type='http', auth='public', methods=['GET'], csrf=False)
+    @http.route(
+        route='/api/hospital/doctor/<int:doctor_id>', type='jsonrpc', auth='public', methods=['GET'], csrf=False
+    )
     def get_doctor(self, doctor_id):
         """Retrieve a doctor by id in the system.
 
@@ -43,29 +40,19 @@ class HospitalController(http.Controller):
         doctor = request.env['hr_hospital.doctor'].sudo().browse(doctor_id)
 
         if not doctor.exists():
-            return request.make_response(
-                json.dumps({'status': 'error', 'message': f'Doctor with ID {doctor_id} not found'}),
-                status=HTTPStatus.NOT_FOUND,
-                headers=[('Content-Type', 'application/json')],
-            )
+            return {'status': 'error', 'data': [], 'message': f'Doctor with ID {doctor_id} not found'}
 
-        doctor_data = {
-            'id': doctor.id,
-            'name': doctor.full_name,
-            'category': doctor.category_id.name if doctor.category_id else 'No Category',
-            'is_intern': doctor.is_intern,
-        }
+        doctor_data = [
+            {
+                'id': doctor.id,
+                'name': doctor.full_name,
+                'category': doctor.category_id.name if doctor.category_id else 'No Category',
+                'is_intern': doctor.is_intern,
+            }
+        ]
 
-        return request.make_response(
-            json.dumps({'status': 'success', 'data': doctor_data}),
-            status=HTTPStatus.OK,
-            headers=[('Content-Type', 'application/json')],
-        )
+        return {'status': 'success', 'data': doctor_data}
 
-    @http.route(route='/api/hospital/doctor', type='http', auth='public', methods=['POST'], csrf=False)
+    @http.route(route='/api/hospital/doctor', type='jsonrpc', auth='public', methods=['POST'], csrf=False)
     def post_doctor(self):
-        return request.make_response(
-            json.dumps({'status': 'success'}),
-            status=HTTPStatus.OK,
-            headers=[('Content-Type', 'application/json')],
-        )
+        return {'status': 'success'}
